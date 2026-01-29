94494940e204c4bfb850403a9e75719a344cca53
JGreen <2501545@uad.ac.uk>
2026-01-29 16:08:07 +0000

    Sheep now has WASD movement
    Bug - Sheep doesn't die

--- Coursework/CMP105App/Player.cpp
+++ Coursework/CMP105App/Player.cpp
@@ -21,7 +21,36 @@ Player::Player()
 void Player::handleInput(float dt)
 {
 
+	Direction last_dir = m_direction;
 
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::A))
+	{
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::W))
+			m_direction = Direction::UP_LEFT;
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::S))
+			m_direction = Direction::DOWN_LEFT;
+		else
+			m_direction = Direction::LEFT;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::D))
+	{
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::W))
+			m_direction = Direction::UP_RIGHT;
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::S))
+			m_direction = Direction::DOWN_RIGHT;
+		else
+			m_direction = Direction::RIGHT;
+	}
+	else
+	{
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::W))
+			m_direction = Direction::UP;
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::S))
+			m_direction = Direction::DOWN;
+	}
+
+	if (m_direction != last_dir)
+		m_inputBuffer = INPUT_BUFFER_LENGTH;
 
 }
 
@@ -30,5 +59,39 @@ void Player::update(float dt)
 
 	m_currentAnimation->animate(dt);
 	setTextureRect(m_currentAnimation->getCurrentFrame());
+
+	if (m_gameOver) return;
+
+	// for diagonal movement
+	float diagonal_speed = m_speed * APPROX_ONE_OVER_ROOT_TWO * dt;
+	float orthog_speed = m_speed * dt;	// orthogonal movement
+
+	switch (m_direction)
+	{
+	case Direction::UP:
+		move({ 0, -orthog_speed });
+		break;
+	case Direction::UP_RIGHT:
+		move({ diagonal_speed, -diagonal_speed });
+		break;
+	case Direction::RIGHT:
+		move({ orthog_speed,0 });
+		break;
+	case Direction::DOWN_RIGHT:
+		move({ diagonal_speed, diagonal_speed });
+		break;
+	case Direction::DOWN:
+		move({ 0, orthog_speed });
+		break;
+	case Direction::DOWN_LEFT:
+		move({ -diagonal_speed, diagonal_speed });
+		break;
+	case Direction::LEFT:
+		move({ -orthog_speed,0 });
+		break;
+	case Direction::UP_LEFT:
+		move({ -diagonal_speed, -diagonal_speed });
+		break;
+	}
 
 }
--- Coursework/CMP105App/Player.h
+++ Coursework/CMP105App/Player.h
@@ -13,6 +13,15 @@ public:
 
 private:
 
+	enum class Direction { UP, DOWN, LEFT, RIGHT, UP_RIGHT, DOWN_RIGHT, DOWN_LEFT, UP_LEFT, NONE };
+	Direction m_direction = Direction::NONE;;
+	float m_speed = 300.0f;
+	float m_inputBuffer = 0.f;
+	bool m_gameOver = false;
+
+	const float INPUT_BUFFER_LENGTH = 0.1f;
+	const float APPROX_ONE_OVER_ROOT_TWO = 0.70710678f;	// 1 / sqrt(2)
+
 	Animation m_walkDown;
 	Animation* m_currentAnimation;
 
